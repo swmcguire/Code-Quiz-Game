0db547bf7c5e4d74264d0fbd951343f2c130334e
swmcguire <swmcguire1@comcast.net>
2022-11-17 17:26:45 -0600

    Updated JavaScript - added buttons - still need to make updates but HUGE breakthroughs

--- Assets/script.js
+++ Assets/script.js
@@ -1,88 +1,147 @@
 var startBtn = document.getElementById('start');
-var highscoreBtn = document.getElementById('highscore');
+var highScoreBtn = document.getElementById('highscore');
+var questionText = document.getElementById('question-text');
+var questionAns = document.getElementById('question-answer');
+var aBtn = document.getElementById('aBtn');
+var bBtn = document.getElementById('bBtn');
+var cBtn = document.getElementById('cBtn');
+var dBtn = document.getElementById('dBtn');
 
-var questions = [
+
+//---------------------------------ARRAY OF QUESTIONS AND ANSWERS------------------------------//
+
+var currentQuestion = 0;
+var questionArr = [
     {
-    question: "Is JavaScript case sensitive",
-    answers: ["Yes","No","Sometimes","If you want it to be"],
-    correct: "Yes"
+        question: "Is JavaScript case sensitive",
+        answers: ["A. Yes", "B. No", "C. Sometimes", "D. If you want it to be"],
+        correct: "A. Yes"
     },
     {
         question: "Which is NOT a correct datatype?",
-        answers: ["String","Boolean","Angelic","Numeric"],
-        correct: "Angelic"
+        answers: ["A. String","B. Boolean","C. Angelic","D. Numeric"],
+        correct: "C. Angelic"
     },
     {
         question: "How do you get the length of var text = \"Lets Go!",
-        answers: ["length.text","len(text)","text.length","text(len)"],
-        correct: "text.length"
+        answers: ["A. length.text","B. len(text)","C. text.length","D. text(len)"],
+        correct: "C. text.length"
     },
     {
         question: "How do you comment out in JavaScript?",
-        answers: ["<!--With this-->","*+With this+*","//With this","----With this"],
-        correct: "//With this"
+        answers: ["A. <!--With this-->","B. *+With this+*","C. //With this","D. ----With this"],
+        correct: "C. //With this"
     },
     {
         question: "Which is correct to set your variable?",
-        answers: ["v length","length = var","var length","i++"],
-        correct: "var length;"
+        answers: ["A. v length","B. length = var","C. var length","D. i++"],
+        correct: "C. var length;"
     },
     {
         question: "Can you build a website with JavaScript Only?",
-        answers: ["Yes","No","You can try","Every good website is."],
-        correct: "No"
+        answers: ["A. Yes","B. No","C. You can try","D. Every good website is."],
+        correct: "B. No"
     },
     {
         question: "Is Java and JavaScript the same thing?",
-        answers: ["Yes","No","If you try really hard","Unsure"],
-        correct: "No"
+        answers: ["A. Yes","B. No","C. If you try really hard","D. Unsure"],
+        correct: "B. No"
     },
     {
         question: "Which HTML element do we put JavaScript in?",
-        answers: ["<js>","<script>","<javaScript>","<h1>"],
-        correct: "<script>"
+        answers: ["A. <js>","B. <script>","C. <javaScript>","D. <h1>"],
+        correct: "B. <script>"
     },
     {
         question: "How do you create a function in JavaScript?",
-        answers: ["function myFunction()","function = myFunctin()","function:myFunction()","function = function"],
-        correct: "function myFunction()"
+        answers: ["A. function myFunction()","B. function = myFunctin()","C. function:myFunction()","D. function = function"],
+        correct: "A. function myFunction()"
     },    
     {
         question: "Which event occurs when the user clicks on an HTML element",
-        answers: ["onclick","onhover","onmouseover","onkeystroke"],
-        correct: "onclick"
+        answers: ["A. onclick","B. onhover","C. onmouseover","D. onkeystroke"],
+        correct: "A. onclick"
     }
 
 ]
 
 
-startBtn.addEventListener('click',start);
-startBtn.addEventListener('click',highscore);
+//---------------------------------EVENT LISTENERS------------------------------//
 
-function start(){
-    console.log("Start!");
+startBtn.addEventListener('click',start);
+//highScoreBtn.addEventListener('click',highscore);
+
+
+
+//---------------------------------THIS FUNCTION STARTS THE GAME------------------------------//
+
+function start() {
+    startTimer();
+    showQuestion();
+
 }
+
+//---------------------------------TIMER FUNCTION------------------------------//
+
+var timerEl = document.querySelector(".timer");
+var gameOverEl = document.getElementById(".game-over");
+
+var secondsLeft = 90;
+
+function startTimer() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = "Timer:  " + secondsLeft + " seconds";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+  }, 1000);
+}
+
+//---------------------------------DISPLAY GAME OVER IF TIMER HITS 0------------------------------//
+
+//Write GameOver on Page
+function gameOver() {
+    gameOverEl.textContent = "GAME OVER!!!!!!";
+  }
+
+
+//---------------------------------THIS FUNCTION STARTS THE GAME------------------------------//
+
+function showQuestion(){
+    currentQuestion = 0;
+    questionText.innerHTML = questionArr[currentQuestion].question;
+
+    console.log(questionArr[currentQuestion].answers[0]);
+    console.log(questionArr[currentQuestion].answers[1]);
+    console.log(questionArr[currentQuestion].answers[2]);
+    console.log(questionArr[currentQuestion].answers[3]);
+
+    aBtn.innerHTML = questionArr[currentQuestion].answers[0];
+    bBtn.innerHTML = questionArr[currentQuestion].answers[1];
+    cBtn.innerHTML = questionArr[currentQuestion].answers[2];
+    dBtn.innerHTML = questionArr[currentQuestion].answers[3];
+}
+
+
+
+
+
+
+
+
+/*
+//---------------------------------HIGH SCORE FUNCTION------------------------------//
 
 function highscore(){
     console.log("Nice...HighScore!");
 }
 
-function startGame() {
-    //this function starts the game 
-    console.log("Inside startGame");
-    startTime();
-    showQuestions();
-}
-
-function startTimer(){
-    console.log("Start timer");
-}
-
-function showQuestion() {
-    console.log ("Show first question");
-}
-
+//---------------------------------GAME OVER FUNCTION------------------------------//
 
 function endGame(){
-    console.log("End the game!");
+   console.log("End the game!");
 }
+*/
