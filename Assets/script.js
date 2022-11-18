84ec724b565037b50a9c6a5b9ca1fdf7761e6e11
swmcguire <swmcguire1@comcast.net>
2022-11-17 22:43:50 -0600

    Updated JavaScript - made adjustments to the if then statements

--- Assets/script.js
+++ Assets/script.js
@@ -15,54 +15,54 @@ var show = document.getElementById('show');
 var currentQuestion = 0;
 var questionArr = [
     {
-        question: "Is JavaScript case sensitive",
+        question: "Is JavaScript case sensitive?",
         answers: ["A. Yes", "B. No", "C. Sometimes", "D. If you want it to be"],
-        correct: "A. Yes"
+        correct: ["A. Yes"]
     },
     {
         question: "Which is NOT a correct datatype?",
         answers: ["A. String","B. Boolean","C. Angelic","D. Numeric"],
-        correct: "C. Angelic"
+        correct: ["C. Angelic"]
     },
     {
-        question: "How do you get the length of var text = \"Lets Go!\"",
+        question: "How do you get the length of var text = \"Lets Go!\"?",
         answers: ["A. length.text","B. len(text)","C. text.length","D. tex(len)"],
-        correct: "C. text.length"
+        correct: ["C. text.length"]
     },
     {
         question: "How do you comment out in JavaScript?",
         answers: ["A. <!--With this-->","B. *+With this+*","C. //With this","D. --With this"],
-        correct: "C. //With this"
+        correct: ["C. //With this"]
     },
     {
         question: "Which is correct to set your variable?",
         answers: ["A. v length","B. length = var","C. var length","D. i++"],
-        correct: "C. var length;"
+        correct: ["C. var length;"]
     },
     {
         question: "Can you build a website with JavaScript Only?",
         answers: ["A. Yes","B. No","C. You can try","D. Obvi"],
-        correct: "B. No"
+        correct: ["B. No"]
     },
     {
         question: "Is Java and JavaScript the same thing?",
         answers: ["A. Yes","B. No","C. If you try hard","D. Unsure"],
-        correct: "B. No"
+        correct: ["B. No"]
     },
     {
         question: "Which type of HTML element do we link JavaScript in?",
         answers: ["A. js","B. script","C. javaScript","D. h1"],
-        correct: "B. <script>"
+        correct: ["B. <script>"]
     },
     {
         question: "How do you create a function in JavaScript?",
         answers: ["A. function myFunction()","B. function = myFunctin()","C. function:myFunction()","D. function = function"],
-        correct: "A. function myFunction()"
+        correct: ["A. function myFunction()"]
     },    
     {
-        question: "Which event occurs when the user clicks on an HTML element",
+        question: "Which event occurs when the user clicks on an HTML element?",
         answers: ["A. onclick","B. onhover","C. onmouseover","D. onkeystroke"],
-        correct: "A. onclick"
+        correct: ["A. onclick"]
     }
 
 ]
@@ -92,13 +92,11 @@ function hideText () {
     startBtn.style.display="none";
     para.style.display="none";
     show.style.display="inline";
-
-    
 }
 
 
-var timerEl = document.querySelector(".timer");
-var gameOverEl = document.getElementById(".game-over");
+var timerEl = document.querySelector('.timer');
+var gameOverEl = document.getElementById('game-over');
 
 var secondsLeft = 90;
 
@@ -134,15 +132,45 @@ function showQuestion(){
     dBtn.innerHTML = questionArr[currentQuestion].answers[3];
 }
 
-/*when button gets clicked it compares to the answer... if the answer == then CORRECT!  
-
-if the answer !== then WRONG!
-THEN 
 
 
+var quizChoice = '';
+
+function getAnswer (choice) { 
+    //var result = document.getElementById("result");
+
+    if (quizChoice === questionArr[currentQuestion].correct[0]) {
+        console.log("Correct!");
+        secondsLeft+=10;
+    } else {
+        secondsLeft-=10;
+        console.log("WRONG!!");
+    }
+
+} 
 
 
-*/
+aBtn.addEventListener("click", () =>{
+    quizChoice = (questionArr[currentQuestion].answers[0]);
+    getAnswer(quizChoice);
+});
+
+
+bBtn.addEventListener("click", () =>{
+    quizChoice =(questionArr[currentQuestion].answers[1]);
+    getAnswer(quizChoice);
+});
+
+cBtn.addEventListener("click", () =>{
+    quizChoice =(questionArr[currentQuestion].answers[2]);
+    getAnswer(quizChoice);
+});
+
+dBtn.addEventListener("click", () =>{
+    quizChoice =(questionArr[currentQuestion].answers[3]);
+    getAnswer(quizChoice);
+});
+
 
 /*
 //---------------------------------HIGH SCORE FUNCTION------------------------------//
